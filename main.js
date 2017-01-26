6f3f5aeaecb4233cdf8c736a942e906afa82d587
Arturo <arturo.luna@tapjoy.com>
2017-01-26 14:23:57 -0800

    main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var canvas          = document.getElementById("canvas");
+canvas.width        = document.body.clientWidth;
+canvas.height       = document.body.clientHeight;
+canvas.style.width  = canvas.width + "px";
+canvas.style.height = canvas.height + "px";
+
+var ctx             = canvas.getContext("2d");
+
+
+// Simple draw method for 30(ish) fps
+setInterval(function() {
+
+}, 33);
