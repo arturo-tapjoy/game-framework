15e675f768c9aa6ba87ff8766e7035b653527e12
Arturo <arturo.luna@tapjoy.com>
2017-01-26 15:00:49 -0800

    adding coditions to class

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ canvas.style.height = canvas.height + "px";
 
 var ctx             = canvas.getContext("2d");
 
-
+// Create a new Rectangle
+var rect     = new Rectangle(15, 15, 50, 50);
 
 // Simple draw method for 30(ish) fps
 setInterval(function() {
