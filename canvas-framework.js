8a8bcc8ae31840aee3dd3b621d35298315a80ddf
Arturo <arturo.luna@tapjoy.com>
2017-01-27 13:32:55 -0800

    update for collision

--- canvas-framework.js
+++ canvas-framework.js
@@ -1,26 +1,72 @@
-Rectangle = function(x, y, w, h) {
+// Rectangle 
+Rectangle = function(x, y, w, h){
 
 
 	if (x == null || y == null || w == null || h == null) {
 		alert("You must pass in all the veriables for a rectange: (x, y, width, height)");
 		
 		var errorMsg = "The following are not provided:";
-		if (x == null) {
+		if (x == null || typeof x !== 'number') {
 			errorMsg += " 'x' ";
 		}
-		if (y == null) {
+		if (y == null || typeof y !== 'number') {
 			errorMsg += " 'y' ";
 		}
-		if (w == null) {
+		if (w == null || '') {
 			errorMsg += " 'width' ";
 		}
-		if (h == null) {
+		if (h == null || '') {
 			errorMsg += " 'height'";
 		}
 		
 		console.log(errorMsg);
 		throw new Error(errorMsg);
 	}	
+
+	this.x		= x;
+	this.y		= y;
+	this.width	= w;
+	this.height	= h;
+
+	// To Check Rectangles or Circles if Intersecting
+	this.Intersects = function(shape) {
+		var offset = 0;
+		if (shape.radius != null) {
+			offset = shape.radius;
+		}
+		// if any of the shapes incoming corners return true. The offset will look for the center point of cirle shape.
+		if (this.Contains(shape.x - offset, shape.y - offset) || this.Contains(shape.x + shape.width - offset, shape.y - offset) ||
+			this.Contains(shape.x - offset, shape.y + shape.height - offset) || this.Contains(shape.x + shape.width - offset, shape.y + shape.height - offset))
+		{
+			return true;
+		}
+		else if (shape.Contains(this.x - offset, this.y - offset) || shape.Contains(this.x + this.width - offset, this.y - offset) ||
+			shape.Contains(this.x - offset, this.y + this.height - offset) || shape.Contains(this.x + this.width - offset, this.y + this.height - offset))
+		{
+			return true;
+		}
+		
+		return false;
+
+
+	};
+
+	// Contains x and y value
+	this.Contains = function(x, y){
+
+	    if (x >= this.x && x <= this.x + this.width && y >= this.y && y <= this.y + this.height)
+			return true;
+		else 
+			return false;
+	};
+
+
+	this.Draw = function(ctx, color){
+
+		ctx.fillStyle = color;
+		ctx.fillRect(this.x, this.y, this.width, this.height)
+
+	};
 
 
 };
--- main.js
+++ main.js
@@ -6,10 +6,35 @@ canvas.style.height = canvas.height + "px";
 
 var ctx             = canvas.getContext("2d");
 
+var movement = 0;
+var rect     = new Rectangle(50, 15, 50, 50);
 // Create a new Rectangle
-var rect     = new Rectangle(15, 15, 50, 50);
+
+console.log(rect);
+
+
+var update = function(){
+
+	rect.x += movement;
+};
+
+var draw = function() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	rect.Draw(ctx, "red");
+};
+
+var step = function(){
+	update();
+	draw();
+
+	window.requestAnimationFrame(step);
+
+};
+
+// call step()
+step();
 
 // Simple draw method for 30(ish) fps
-setInterval(function() {
+// setInterval(function() {
 
-}, 33);
+// }, 33);
